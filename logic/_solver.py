4c234f40f0d0c2f06baad779515b23e19f56df87
chrsec <stronka.k@gmail.com>
2021-05-23 09:44:02 +0200

    implement solver for 'regular' suddoku (crossing out sufficient)

--- logic/_solver.py
+++ logic/_solver.py
@@ -12,15 +12,15 @@ def create_candidates_stack():
 def cross_out_sudoku(stack: numpy.array, sudoku: numpy.array) -> None:
     for i, j in product(range(0, 9), range(0, 9)):
         number = sudoku[i, j]
+        if number:
+            k = int(number - 1)
+            i1, i2 = find_box_coords(i)
+            j1, j2 = find_box_coords(j)
 
-        k = int(number - 1)
-        i1, i2 = find_box_coords(i)
-        j1, j2 = find_box_coords(j)
-
-        stack[k, i, :] = 0
-        stack[k, :, j] = 0
-        stack[:, i, j] = 0
-        stack[k, i1:i2, j1:j2] = 0
+            stack[k, i, :] = 0
+            stack[k, :, j] = 0
+            stack[:, i, j] = 0
+            stack[k, i1:i2, j1:j2] = 0
 
     return
 
--- test/test_solver.py
+++ test/test_solver.py
@@ -74,7 +74,7 @@ class TestSolver(unittest.TestCase):
         cross_out_sudoku(candidate_stack, sudoku)
         self.assertNumpyEqual(expected, candidate_stack[1, 0:3, 0:3])
 
-    def test_CrossOutSudoku_SudokuEmptyWithOneAndTwoInSecondRow_CrossOutSecondBox(self):
+    def test_CrossOutSudoku_SudokuEmptyWithOneAndTwoInFirstRow_CrossOutSecondBox(self):
         candidate_stack = create_candidates_stack()
         sudoku = numpy.zeros((9, 9))
         sudoku[0, 0] = 1
@@ -102,13 +102,13 @@ class TestSolver(unittest.TestCase):
         cross_out_sudoku(candidate_stack, sudoku)
         self.assertNumpyEqual(expected, candidate_stack[1, 0:3, 3:6])
 
-    def test_CrossOutSudoku_SudokuEmptyWithOneAndTwoInSecondRow_CrossWholeStack(self):
+    def test_CrossOutSudoku_SudokuEmptyWithOne_CrossWholeStack(self):
         candidate_stack = create_candidates_stack()
         sudoku = numpy.zeros((9, 9))
         sudoku[0, 0] = 1
         expected = numpy.array([0, 0, 0,  0, 0, 0,  0, 0, 0])
         cross_out_sudoku(candidate_stack, sudoku)
-        self.assertNumpyEqual(expected, candidate_stack[:, 0, 3])
+        self.assertNumpyEqual(expected, candidate_stack[:, 0, 0])
 
     def test_CrossOutSudoku_SudokuEmptyWithOneAndTwoInFourthRow_CrossOutSecondBox(self):
         candidate_stack = create_candidates_stack()
@@ -123,6 +123,15 @@ class TestSolver(unittest.TestCase):
 
         cross_out_sudoku(candidate_stack, sudoku)
         self.assertNumpyEqual(expected, candidate_stack[1, 3:6, 3:6])
+
+    def test_CrossOutSudoku_SudokuWithOneAndEmptyFirstCell_CandidateStackOverFirstCellIsCorrect(self):
+        candidate_stack = create_candidates_stack()
+        sudoku = numpy.zeros((9, 9))
+        sudoku[1, 1] = 1
+        expected = numpy.array([0, 2, 3,  4, 5, 6,  7, 8, 9])
+
+        cross_out_sudoku(candidate_stack, sudoku)
+        self.assertNumpyEqual(expected, candidate_stack[:, 0, 0])
 
     def test_CreateSudokuFill_StackIsNonDefinitive_FillEmpty(self):
         candidate_stack = create_candidates_stack()
@@ -155,10 +164,9 @@ class TestSolver(unittest.TestCase):
         fill = create_sudoku_fill(candidate_stack, sudoku)
         self.assertNumpyEqual(expected, fill)
 
-    #
-    # def test_SolveSudoku_Regular_SudokuCorrect(self):
-    #     sudoku = self.get_regular_sudoku()
-    #     self.assertTrue(check_sudoku_correct(solve_sudoku(sudoku)))
+    def test_SolveSudoku_Regular_SudokuCorrect(self):
+        sudoku = self.get_regular_sudoku()
+        self.assertTrue(check_sudoku_correct(solve_sudoku(sudoku)))
 
     @staticmethod
     def get_regular_sudoku():
