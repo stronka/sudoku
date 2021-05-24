fbb4b18d7a200c44f56a07b0b378211a667238c1
chrsec <stronka.k@gmail.com>
2021-05-25 00:14:38 +0200

    Add test case for repeating values in box

--- test/test_logic.py
+++ test/test_logic.py
@@ -80,3 +80,17 @@ class TestLogic(unittest.TestCase):
             [6, 7, 9, 8, 5, 1, 3, 2, 4]
         ])
         self.assertFalse(check_sudoku_correct(sudoku))
+
+    def test_checkSudokuCorrect_ValuesRepeatInBox_ReturnFalse(self):
+        sudoku = numpy.array([
+            [7, 8, 2, 6, 4, 3, 9, 1, 5],
+            [1, 3, 5, 7, 8, 2, 4, 6, 9],
+            [8, 6, 4, 1, 9, 5, 3, 2, 7],
+            [9, 1, 6, 5, 3, 7, 8, 4, 2],
+            [2, 4, 7, 9, 6, 8, 1, 5, 3],
+            [5, 9, 3, 2, 1, 4, 6, 7, 8],
+            [4, 2, 9, 8, 7, 6, 5, 3, 1],
+            [3, 5, 1, 4, 2, 9, 7, 8, 6],
+            [6, 7, 8, 3, 5, 1, 2, 9, 4]
+        ])
+        self.assertFalse(check_sudoku_correct(sudoku))
