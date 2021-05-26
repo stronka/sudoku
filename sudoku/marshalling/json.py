896f97d833f286173e99f70518b0c875cad28d1a
chrsec <stronka.k@gmail.com>
2021-05-26 22:33:38 +0200

    Add json marshalling

--- sudoku/marshalling/json.py
+++ sudoku/marshalling/json.py
@@ -6,3 +6,10 @@ import numpy
 def unmarshall_json(data: str) -> numpy.array:
     parsed = json.loads(data)
     return numpy.array(parsed['sudoku'])
+
+
+def marshall_json(data: numpy.array) -> str:
+    json_dict = {
+        'sudoku': numpy.ndarray.tolist(data)
+    }
+    return json.dumps(json_dict)
--- sudoku/tests/unit/marshalling/test_json.py
+++ sudoku/tests/unit/marshalling/test_json.py
@@ -2,7 +2,7 @@ import unittest
 import numpy
 from numpy.testing import assert_equal
 
-from sudoku.marshalling.json import unmarshall_json
+from sudoku.marshalling.json import unmarshall_json, marshall_json
 
 
 class TestUnmarshallJson(unittest.TestCase):
@@ -38,3 +38,34 @@ class TestUnmarshallJson(unittest.TestCase):
             [2, 0, 0,    0, 0, 0,   5, 0, 0]
         ])
         assert_equal(expected, unmarshall_json(data))
+
+
+    def test_Marshall_Always_ReturnJson(self):
+        data = numpy.array([
+            [0, 0, 2,    0, 0, 0,   0, 0, 8],
+            [0, 5, 0,    0, 0, 0,   7, 0, 0],
+            [4, 0, 0,    0, 0, 1,   6, 3, 0],
+
+            [0, 0, 0,    4, 0, 0,   3, 7, 2],
+            [0, 0, 0,    9, 0, 3,   0, 0, 0],
+            [1, 2, 3,    0, 0, 6,   0, 0, 0],
+
+            [0, 8, 9,    5, 0, 0,   0, 0, 4],
+            [0, 0, 5,    0, 0, 0,   0, 9, 0],
+            [2, 0, 0,    0, 0, 0,   5, 0, 0]
+        ])
+        expected = \
+            '{"sudoku": ' \
+                '[' \
+                    '[0, 0, 2, 0, 0, 0, 0, 0, 8], ' \
+                    '[0, 5, 0, 0, 0, 0, 7, 0, 0], ' \
+                    '[4, 0, 0, 0, 0, 1, 6, 3, 0], ' \
+                    '[0, 0, 0, 4, 0, 0, 3, 7, 2], ' \
+                    '[0, 0, 0, 9, 0, 3, 0, 0, 0], ' \
+                    '[1, 2, 3, 0, 0, 6, 0, 0, 0], ' \
+                    '[0, 8, 9, 5, 0, 0, 0, 0, 4], ' \
+                    '[0, 0, 5, 0, 0, 0, 0, 9, 0], ' \
+                    '[2, 0, 0, 0, 0, 0, 5, 0, 0]' \
+                ']' \
+            '}'
+        self.assertEqual(expected, marshall_json(data))
