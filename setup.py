bd4997e1baccfd5867d90174051dba8d74d0d3d1
chrsec <stronka.k@gmail.com>
2021-05-26 23:38:31 +0200

    adjust setup

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name='sudoku',
     version='0.1',
-    packages=[
-        'sudoku',
-        'sudoku.logic',
-        'sudoku.logic.utils',
-        'sudoku.logic.heuristics',
-        'sudoku.tests',
-        'sudoku.tests.unit',
-        'sudoku.tests.unit.parser',
-        'sudoku.tests.unit.parser.json',
-        'sudoku.parser',
-        'sudoku.parser.json'
-    ],
+    packages=find_packages(),
     url='https://github.com/stronka/sudoku',
     license='',
     author='stronka',
