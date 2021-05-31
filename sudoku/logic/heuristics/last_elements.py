85350b46e56281de99ca937c58bf3a3448cf6d68
chrsec <stronka.k@gmail.com>
2021-05-31 18:28:55 +0200

    Add solution logging in row-wise last elements processing heuristic

--- sudoku/logic/heuristics/last_elements.py
+++ sudoku/logic/heuristics/last_elements.py
@@ -1,19 +1,36 @@
 import numpy
 
+_ACTION = "Remove {}"
+_REASON_ROW = "Last position in row: Cell ({}, {}) is last possible location for number {}"
+_REASON_COL = "Last position in col: Cell ({}, {}) is last possible location for number {}"
 
-def process_last_elements(stack: numpy.array, *args) -> None:
+
+def process_last_elements(stack: numpy.array, *args, **kwargs) -> None:
+    solution_log = kwargs.setdefault('solution_log', None)
+
     for k in range(0, 9):
         for i in range(0, 9):
             row = stack[k, i, :]
             if row.sum() and row.sum()/row.max() == 1:
                 ind = numpy.nonzero(row)[0]
+                number = int(k+1)
+                log_solution_steps(solution_log, number, (i, ind[0]), _REASON_ROW)
+
                 stack[:, i, ind] = 0
-                stack[k, i, ind] = k+1
+                stack[k, i, ind] = number
 
         for j in range(0, 9):
             col = stack[k, :, j]
             if col.sum() and col.sum()/col.max() == 1:
                 ind = numpy.nonzero(col)[0]
+                number = int(k+1)
                 stack[:, ind, j] = 0
-                stack[k, ind, j] = k+1
+                stack[k, ind, j] = number
     return
+
+
+def log_solution_steps(solution, number, cell, reason):
+    if solution:
+        for candidate in range(1, 10):
+            if candidate != number:
+                solution.add_step(cell, _ACTION.format(candidate), reason.format(cell[0], cell[1], number))
--- sudoku/tests/unit/logic/heuristics/test_last_elements.py
+++ sudoku/tests/unit/logic/heuristics/test_last_elements.py
@@ -2,6 +2,7 @@ import unittest
 import numpy
 from sudoku.logic._solver import create_candidates_stack
 from sudoku.logic.heuristics.last_elements import process_last_elements
+from sudoku.logic.meta.solution_log import SolutionLog
 
 
 class TestLastElementsHeuristic(unittest.TestCase):
@@ -81,6 +82,26 @@ class TestLastElementsHeuristic(unittest.TestCase):
 
         process_last_elements(candidate_stack)
         self.assertNumpyEqual(expected, candidate_stack[:, 4, 5])
+
+    def test_ProcessLastElements_LastElementInRowFound_LogWipeAlongStack(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[4, 0, :] = 0
+        candidate_stack[4, 0, 4] = 5
+
+        expected = [
+            {'cell': (0, 4), 'action': "Remove 1", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 2", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 3", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 4", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 6", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 7", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 8", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+            {'cell': (0, 4), 'action': "Remove 9", 'reason': "Last position in row: Cell (0, 4) is last possible location for number 5"},
+        ]
+
+        process_last_elements(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.where.query('cell == (0, 4)').get_steps())
 
     @staticmethod
     def assertNumpyEqual(first: numpy.array, second: numpy):
