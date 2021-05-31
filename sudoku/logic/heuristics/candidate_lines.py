89d5d7fea3b09c1709f9f788019cae28e17345d6
chrsec <stronka.k@gmail.com>
2021-05-31 22:19:59 +0200

    Candidate line row iteration over first box

--- sudoku/logic/heuristics/candidate_lines.py
+++ sudoku/logic/heuristics/candidate_lines.py
@@ -1,3 +1,10 @@
+import numpy
+
 
 def process_candidate_lines(stack: numpy.array, *args, **kwargs) -> None:
+    for row in range(9):
+        box_row = stack[3, row, 0:3]
+        if box_row.sum() / box_row.max() == 2:
+            stack[3, row, 3:9] = 0
+
     return
--- sudoku/tests/unit/logic/heuristics/test_candidate_line.py
+++ sudoku/tests/unit/logic/heuristics/test_candidate_line.py
@@ -6,9 +6,36 @@ from sudoku.logic.heuristics.candidate_lines import process_candidate_lines
 
 
 class TestLastElementsHeuristic(unittest.TestCase):
-    def test_ProcessLastElements_FullCandidateStack_DoNothing(self):
+    def test_ProcessCandidateLines_FullCandidateStack_DoNothing(self):
         candidate_stack = create_candidates_stack()
         expected = candidate_stack
 
         process_candidate_lines(candidate_stack)
         numpy.testing.assert_equal(expected, candidate_stack)
+
+    def test_ProcessCandidateLines_CandidateLinePresent_RemoveOtherInThatRow(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 0, :] = [4, 4, 0,  0, 4, 0,  0, 0, 4]
+
+        expected = [4, 4, 0,  0, 0, 0,  0, 0, 0]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 0, :])
+
+    def test_ProcessCandidateLines_CandidateLinePresentInSecondRow_RemoveOtherInThatRow(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 1, :] = [4, 4, 0,  0, 4, 0,  0, 0, 4]
+
+        expected = [4, 4, 0,  0, 0, 0,  0, 0, 0]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
+
+    def test_ProcessCandidateLines_CandidatesFormingLinePresentInSecondRowSecondThirdCol_RemoveOtherInThatRow(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 1, :] = [0, 4, 4,  0, 4, 0,  0, 0, 4]
+
+        expected = [0, 4, 4,  0, 0, 0,  0, 0, 0]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
