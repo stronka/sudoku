afefb3779c056bfb2d1947322efe914ca5f63fa3
chrsec <stronka.k@gmail.com>
2021-06-01 19:09:45 +0200

    Complete candidate line processing

--- sudoku/logic/heuristics/candidate_lines.py
+++ sudoku/logic/heuristics/candidate_lines.py
@@ -1,10 +1,50 @@
+import copy
+
 import numpy
 
 
 def process_candidate_lines(stack: numpy.array, *args, **kwargs) -> None:
-    for row in range(9):
-        box_row = stack[3, row, 0:3]
-        if box_row.sum() / box_row.max() == 2:
-            stack[3, row, 3:9] = 0
+    for candidate in range(9):
+        for row in range(9):
+
+            done = False
+            rollback = False
+            initial_stack_row = copy.copy(stack[candidate, row, :])
+
+            for b in range(3):
+                box_row = initial_stack_row[3*b:3*b+3]
+
+                if box_row.sum() / box_row.max() == 2:
+                    if done:
+                        rollback = True
+                        break
+
+                    stack[candidate, row, 0:3*b] = 0
+                    stack[candidate, row, 3*b+3:9] = 0
+                    done = True
+
+            if rollback:
+                stack[candidate, row, :] = initial_stack_row
+
+        for col in range(9):
+
+            done = False
+            rollback = False
+            initial_stack_col = copy.copy(stack[candidate, :, col])
+
+            for b in range(3):
+                box_col = initial_stack_col[3*b:3*b+3]
+
+                if box_col.sum() / box_col.max() == 2:
+                    if done:
+                        rollback = True
+                        break
+
+                    stack[candidate, 0:3*b, col] = 0
+                    stack[candidate, 3*b+3:9, col] = 0
+                    done = True
+
+            if rollback:
+                stack[candidate, :, col] = initial_stack_col
 
     return
--- sudoku/tests/unit/logic/heuristics/test_candidate_line.py
+++ sudoku/tests/unit/logic/heuristics/test_candidate_line.py
@@ -48,3 +48,66 @@ class TestLastElementsHeuristic(unittest.TestCase):
 
         process_candidate_lines(candidate_stack)
         numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
+
+    def test_ProcessCandidateLines_CandidatesFormingLinePresentInSecondRowSecondBox_RemoveOtherInThatRow(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 1, :] = [0, 4, 0,  4, 4, 0,  0, 0, 4]
+
+        expected = [0, 0, 0,  4, 4, 0,  0, 0, 0]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
+
+    def test_ProcessCandidateLines_CandidatesFormingLineInMoreBoxes_DoNothing(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 1, :] = [0, 4, 4,  4, 4, 0,  0, 0, 4]
+
+        expected = [0, 4, 4,  4, 4, 0,  0, 0, 4]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
+
+    def test_ProcessCandidateLines_CandidatesFormingLineInMoreBoxes2_DoNothing(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 1, :] = [0, 4, 4,  4, 4, 0,  4, 0, 4]
+
+        expected = [0, 4, 4,  4, 4, 0,  4, 0, 4]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
+
+    def test_ProcessCandidateLines_CandidatesFormingLineLastBox_WipeOthersInRow(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[3, 1, :] = [0, 4, 0,  0, 4, 0,  4, 0, 4]
+
+        expected = [0, 0, 0,  0, 0, 0,  4, 0, 4]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[3, 1, :])
+
+    def test_ProcessCandidateLines_OtherCandidateNumber_WipeOthersInRow(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[4, 1, :] = [0, 5, 0,  0, 5, 0,  5, 0, 5]
+
+        expected = [0, 0, 0,  0, 0, 0,  5, 0, 5]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[4, 1, :])
+
+    def test_ProcessCandidateLines_LineInColumn_WipeOthersInColumn(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[4, :, 5] = [0, 5, 0,  0, 5, 0,  5, 0, 5]
+
+        expected = [0, 0, 0,  0, 0, 0,  5, 0, 5]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[4, :, 5])
+
+    def test_ProcessCandidateLines_CandidatePairsInTwoBoxes_DoNothing(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[4, :, 3] = [0, 5, 0,  5, 5, 0,  5, 0, 5]
+
+        expected = [0, 5, 0,  5, 5, 0,  5, 0, 5]
+
+        process_candidate_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[4, :, 3])
