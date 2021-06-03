aba1d000de5b3ff1f667fc467734a58692789f27
chrsec <stronka.k@gmail.com>
2021-06-03 13:19:34 +0200

    Complete columnwise double pair processing

--- sudoku/logic/heuristics/double_pairs.py
+++ sudoku/logic/heuristics/double_pairs.py
@@ -1,32 +1,35 @@
+import itertools
+
 import numpy
 
 from sudoku.logic.utils.utils import assert_line_count
 
 
 def process_double_pairs(stack: numpy.array) -> None:
-    candidate_layer = stack[1, :, :]
-    top_box = candidate_layer[0:3, 3:6]
-    mid_box = candidate_layer[3:6, 3:6]
-    bottom_box = candidate_layer[6:9, 3:6]
+    for candidate in range(9):
+        candidate_layer = stack[candidate, :, :]
+        bands = ((0, 3), (3, 6), (6, 9))
 
-    if assert_line_count(top_box.flatten(), 2):
-        if assert_line_count(mid_box.flatten(), 2):
-            first_line = numpy.hstack((top_box[:, 0], mid_box[:, 0]))
-            third_line = numpy.hstack((top_box[:, 2], mid_box[:, 2]))
-            if assert_line_count(first_line, 2) and assert_line_count(third_line, 2):
-                bottom_box[:, 0] = 0
-                bottom_box[:, 2] = 0
-    #
-    # stack[1, :, :] = numpy.array([
-    #     [0, 0, 0,    2, 0, 2,   0, 0, 0],
-    #     [0, 0, 0,    0, 0, 0,   0, 0, 0],
-    #     [0, 0, 0,    0, 0, 0,   0, 0, 0],
-    #
-    #     [0, 0, 0,    0, 0, 0,   0, 0, 0],
-    #     [0, 0, 0,    2, 0, 2,   0, 0, 0],
-    #     [0, 0, 0,    0, 0, 0,   0, 0, 0],
-    #
-    #     [0, 0, 0,    0, 0, 0,   0, 0, 0],
-    #     [0, 0, 0,    0, 2, 0,   0, 0, 0],
-    #     [0, 0, 0,    0, 2, 0,   0, 0, 0],
-    # ])
+        for band in bands:
+            for ids1, ids2, ids3 in itertools.permutations(bands):
+                scan_boxes_columnwise(ids1, ids2, ids3, band, candidate_layer)
+
+
+def scan_boxes_columnwise(ids1, ids2, ids3, band, candidate_layer):
+    band_slice = slice(*band)
+    first_box = candidate_layer[slice(*ids1), band_slice]
+    second_box = candidate_layer[slice(*ids2), band_slice]
+    third_box = candidate_layer[slice(*ids3), band_slice]
+
+    if assert_line_count(first_box.flatten(), 2) and assert_line_count(second_box.flatten(), 2):
+        for first_col, second_col, _ in itertools.permutations((0, 1, 2)):
+            scan_columns(first_col, second_col, first_box, second_box, third_box)
+
+
+def scan_columns(id1, id2, top_box, mid_box, bottom_box):
+    first_line = numpy.hstack((top_box[:, id1], mid_box[:, id1]))
+    second_line = numpy.hstack((top_box[:, id2], mid_box[:, id2]))
+
+    if assert_line_count(first_line, 2) and assert_line_count(second_line, 2):
+        bottom_box[:, id1] = 0
+        bottom_box[:, id2] = 0
--- sudoku/tests/unit/logic/heuristics/test_double_pair.py
+++ sudoku/tests/unit/logic/heuristics/test_double_pair.py
@@ -72,3 +72,169 @@ class TestDoublePairHeuristic(unittest.TestCase):
 
         process_double_pairs(candidate_stack)
         numpy.testing.assert_equal(expected, candidate_stack[1, :, :])
+
+    def test_DoublePairs_DoublePairsInRowsZeroAndFiveColumnsThreeAndFour_WipeAlongStack(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[1, :, :] = numpy.array([
+            [0, 0, 0,    2, 2, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    2, 2, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    2, 2, 2,   0, 0, 0],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,    2, 2, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    2, 2, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 2,   0, 0, 0],
+            [0, 0, 0,    0, 0, 2,   0, 0, 0],
+        ])
+
+        process_double_pairs(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[1, :, :])
+
+    def test_DoublePairs_DoublePairsInRowsZeroAndFiveColumnsFourAndFive_WipeAlongStack(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[1, :, :] = numpy.array([
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    2, 2, 2,   0, 0, 0],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    2, 0, 0,   0, 0, 0],
+        ])
+
+        process_double_pairs(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[1, :, :])
+
+    def test_DoublePairs_DoublePairsInRowsZeroAndEightColumnsFourAndFive_WipeAlongStack(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[1, :, :] = numpy.array([
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 2, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    2, 2, 2,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    2, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 2, 2,   0, 0, 0],
+        ])
+
+        process_double_pairs(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[1, :, :])
+
+
+    def test_DoublePairs_DoublePairsInRowsZeroAndEightColumnsSevenAndEight_WipeAlongStack(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[1, :, :] = numpy.array([
+            [0, 0, 0,    0, 0, 0,   0, 2, 2],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 2, 0],
+            [0, 0, 0,    0, 0, 0,   0, 2, 2],
+            [0, 0, 0,    0, 0, 0,   2, 2, 2],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 2, 2],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,    0, 0, 0,   0, 2, 2],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   2, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 2, 2],
+        ])
+
+        process_double_pairs(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[1, :, :])
+
+    def test_DoublePairs_DoublePairsInRowsZeroAndEightColumnsSevenAndEightLayerTwo_WipeAlongStack(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[2, :, :] = numpy.array([
+            [0, 0, 0,    0, 0, 0,   0, 3, 3],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 3, 0],
+            [0, 0, 0,    0, 0, 0,   0, 3, 3],
+            [0, 0, 0,    0, 0, 0,   3, 3, 3],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 3, 3],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,    0, 0, 0,   0, 3, 3],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   3, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 3, 3],
+        ])
+
+        process_double_pairs(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[2, :, :])
