300944bf424caff48fd5ecfad6f38efd867870f2
chrsec <stronka.k@gmail.com>
2021-06-03 13:45:58 +0200

    Refactor scan box lines

--- sudoku/logic/heuristics/double_pairs.py
+++ sudoku/logic/heuristics/double_pairs.py
@@ -12,8 +12,32 @@ def process_double_pairs(stack: numpy.array) -> None:
 
         for band in bands:
             for ids1, ids2, ids3 in itertools.permutations(bands):
-                scan_boxes_rowwise(ids1, ids2, ids3, band, candidate_layer)
-                scan_boxes_columnwise(ids1, ids2, ids3, band, candidate_layer)
+                scan_box_lines(ids1, ids2, ids3, band, candidate_layer, extract_box_along_rows, scan_rows)
+                scan_box_lines(ids1, ids2, ids3, band, candidate_layer, extract_box_along_columns, scan_columns)
+
+
+def scan_box_lines(ids1, ids2, ids3, band, candidate_layer, extract_method, scan_method):
+    first_box, second_box, third_box = extract_method(ids1, ids2, ids3, band, candidate_layer)
+
+    if assert_line_count(first_box.flatten(), 2) and assert_line_count(second_box.flatten(), 2):
+        for first_line, second_line, _ in itertools.permutations((0, 1, 2)):
+            scan_method(ids1, ids2, ids3, first_line, second_line, first_box, second_box, third_box)
+
+
+def extract_box_along_rows(ids1, ids2, ids3, band, candidate_layer):
+    band_slice = slice(*band)
+    first_box = candidate_layer[band_slice, slice(*ids1)]
+    second_box = candidate_layer[band_slice, slice(*ids2)]
+    third_box = candidate_layer[band_slice, slice(*ids3)]
+    return first_box, second_box, third_box
+
+
+def extract_box_along_columns(ids1, ids2, ids3, band, candidate_layer):
+    band_slice = slice(*band)
+    first_box = candidate_layer[slice(*ids1), band_slice]
+    second_box = candidate_layer[slice(*ids2), band_slice]
+    third_box = candidate_layer[slice(*ids3), band_slice]
+    return first_box, second_box, third_box
 
 
 def scan_rows(first_band, second_band, third_band, first_line_id, second_line_id, first_box, second_box, third_box):
