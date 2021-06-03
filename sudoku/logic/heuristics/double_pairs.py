7464ef7332ec45d6a29d449fe5d56372a33b293d
chrsec <stronka.k@gmail.com>
2021-06-03 14:43:49 +0200

    Implement row logging in double pair heuristic

--- sudoku/logic/heuristics/double_pairs.py
+++ sudoku/logic/heuristics/double_pairs.py
@@ -4,24 +4,30 @@ import numpy
 
 from sudoku.logic.utils.utils import assert_line_count
 
+_ACTION = "Remove {}"
 
-def process_double_pairs(stack: numpy.array) -> None:
+_REASON_ROW = "Double pair: candidates in cells ({0}, {2}), ({0}, {3}), ({1}, {2}), ({1}, {3}) form a line along row {4}."
+
+
+def process_double_pairs(stack: numpy.array, *args, **kwargs) -> None:
+    solution_log = kwargs.setdefault('solution_log', None)
+
     for candidate in range(9):
         candidate_layer = stack[candidate, :, :]
         bands = ((0, 3), (3, 6), (6, 9))
 
         for band in bands:
             for ids1, ids2, ids3 in itertools.permutations(bands):
-                _scan_box_lines(ids1, ids2, ids3, band, candidate_layer, _extract_box_along_rows, _scan_line_across_rows)
-                _scan_box_lines(ids1, ids2, ids3, band, candidate_layer, _extract_box_along_columns, _scan_across_columns)
+                _scan_box_lines(candidate, ids1, ids2, ids3, band, candidate_layer, solution_log, _extract_box_along_rows, _scan_line_across_rows)
+                _scan_box_lines(candidate, ids1, ids2, ids3, band, candidate_layer, solution_log, _extract_box_along_columns, _scan_across_columns)
 
 
-def _scan_box_lines(ids1, ids2, ids3, band, candidate_layer, extract_method, scan_method):
+def _scan_box_lines(candidate, ids1, ids2, ids3, band, candidate_layer, solution_log, extract_method, scan_method):
     first_box, second_box, third_box = extract_method(ids1, ids2, ids3, band, candidate_layer)
 
     if assert_line_count(first_box.flatten(), 2) and assert_line_count(second_box.flatten(), 2):
         for first_line, second_line, _ in itertools.permutations((0, 1, 2)):
-            scan_method(ids1, ids2, ids3, first_line, second_line, first_box, second_box, third_box)
+            scan_method(candidate, band, ids1, ids2, ids3, first_line, second_line, first_box, second_box, third_box, solution_log)
 
 
 def _extract_box_along_rows(ids1, ids2, ids3, band, candidate_layer):
@@ -40,16 +46,44 @@ def _extract_box_along_columns(ids1, ids2, ids3, band, candidate_layer):
     return first_box, second_box, third_box
 
 
-def _scan_line_across_rows(first_band, second_band, third_band, first_line_id, second_line_id, first_box, second_box, third_box):
+def _scan_line_across_rows(candidate, row_band, first_band, second_band, third_band, first_line_id, second_line_id, first_box, second_box, third_box, solution_log):
     first_line = numpy.hstack((first_box[first_line_id, :], second_box[first_line_id, :]))
     second_line = numpy.hstack((first_box[second_line_id, :], second_box[second_line_id, :]))
 
     if assert_line_count(first_line, 2) and assert_line_count(second_line, 2):
-        third_box[first_line_id, :] = 0
-        third_box[second_line_id, :] = 0
+        nonzeros_first_line = numpy.argwhere(third_box[first_line_id, :] != 0)
+        third_box[first_line_id, nonzeros_first_line] = 0
+
+        nonzeros_second_line = numpy.argwhere(third_box[second_line_id, :] != 0)
+        third_box[second_line_id, nonzeros_second_line] = 0
+
+        if solution_log:
+            global_first_line_id = row_band[0] + first_line_id
+            global_second_line_id = row_band[0] + second_line_id
+            global_first_pair_col_id = first_band[0] + first_box[first_line_id, :].nonzero()[0][0]
+            global_second_pair_col_id = second_band[0] + second_box[first_line_id, :].nonzero()[0][0]
+
+            _log_row_remove(solution_log, candidate, global_first_line_id, nonzeros_first_line, global_first_line_id,
+                            global_first_pair_col_id, global_second_line_id, global_second_pair_col_id, third_band)
+
+            _log_row_remove(solution_log, candidate, global_second_line_id, nonzeros_second_line, global_first_line_id,
+                            global_first_pair_col_id, global_second_line_id, global_second_pair_col_id, third_band)
 
 
-def _scan_across_columns(first_band, second_band, third_band, first_line_id, second_line_id, first_box, second_box, third_box):
+def _log_row_remove(solution_log, candidate, removed_line_id, nonzeros_first_line, global_first_line_id,
+                    global_first_pair_col_id, global_second_line_id, global_second_pair_col_id, third_band):
+    for _id in nonzeros_first_line:
+        global_removed_id = third_band[0] + _id[0]
+
+        fmt_reason = _REASON_ROW.format(global_first_line_id, global_second_line_id, global_first_pair_col_id,
+                                        global_second_pair_col_id, removed_line_id)
+
+        solution_log.add_step(
+            (removed_line_id, global_removed_id), _ACTION.format(int(candidate + 1)), fmt_reason
+        )
+
+
+def _scan_across_columns(candidate, col_band, first_band, second_band, third_band, first_line_id, second_line_id, first_box, second_box, third_box, solution_log):
     first_line = numpy.hstack((first_box[:, first_line_id], second_box[:, first_line_id]))
     second_line = numpy.hstack((first_box[:, second_line_id], second_box[:, second_line_id]))
 
--- sudoku/tests/unit/logic/heuristics/test_double_pair.py
+++ sudoku/tests/unit/logic/heuristics/test_double_pair.py
@@ -4,6 +4,7 @@ import numpy
 
 from sudoku.logic._solver import create_candidates_stack
 from sudoku.logic.heuristics.double_pairs import process_double_pairs
+from sudoku.logic.meta.solution_log import SolutionLog
 
 
 class TestDoublePairHeuristic(unittest.TestCase):
@@ -271,3 +272,139 @@ class TestDoublePairHeuristic(unittest.TestCase):
 
         process_double_pairs(candidate_stack)
         numpy.testing.assert_equal(expected, candidate_stack[2, :, :])
+
+    def test_DoublePairs_DoublePairsInRow_LogSolution(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[2, :, :] = numpy.array([
+            [3, 0, 0,    0, 3, 0,   0, 3, 0],
+            [0, 3, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 3, 0,   0, 3, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (0, 0),
+                'action': "Remove 3",
+                'reason': "Double pair: candidates in cells (0, 4), (0, 7), (2, 4), (2, 7) form a line along row 0."},
+        ]
+
+        process_double_pairs(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
+
+    def test_DoublePairs_DoublePairsInRowLayerThree_LogSolution(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[3, :, :] = numpy.array([
+            [4, 0, 0,    0, 4, 0,   0, 4, 0],
+            [0, 4, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 4, 0,   0, 4, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (0, 0),
+                'action': "Remove 4",
+                'reason': "Double pair: candidates in cells (0, 4), (0, 7), (2, 4), (2, 7) form a line along row 0."},
+        ]
+
+        process_double_pairs(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
+
+    def test_DoublePairs_DoublePairsInRowLayerThree_LogSolutionInRowTwo(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[3, :, :] = numpy.array([
+            [0, 0, 0,    0, 4, 0,   0, 4, 0],
+            [0, 4, 0,    0, 0, 0,   0, 0, 0],
+            [4, 0, 0,    0, 4, 0,   0, 4, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (2, 0),
+                'action': "Remove 4",
+                'reason': "Double pair: candidates in cells (0, 4), (0, 7), (2, 4), (2, 7) form a line along row 2."},
+        ]
+
+        process_double_pairs(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
+
+    def test_DoublePairs_DoublePairsInRowSecondBoxWipedLayerThree_LogSolutionInRowTwo(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[3, :, :] = numpy.array([
+            [4, 0, 0,    0, 0, 0,   0, 4, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [4, 0, 0,    0, 4, 0,   0, 4, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (2, 4),
+                'action': "Remove 4",
+                'reason': "Double pair: candidates in cells (0, 0), (0, 7), (2, 0), (2, 7) form a line along row 2."},
+        ]
+
+        process_double_pairs(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
+
+    def test_DoublePairs_DoublePairsInRowSecondBandThirdBoxWipedLayerThree_LogSolutionInRowTwo(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[3, :, :] = numpy.array([
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+            [4, 0, 0,    0, 4, 0,   0, 4, 0],
+            [4, 0, 0,    0, 4, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+
+
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+            [0, 0, 0,    0, 0, 0,   0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (3, 7),
+                'action': "Remove 4",
+                'reason': "Double pair: candidates in cells (3, 0), (3, 4), (4, 0), (4, 4) form a line along row 3."},
+        ]
+
+        process_double_pairs(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
