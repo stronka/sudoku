43a304b3ad57bdddc3926b7111c7db5dbb05ce79
chrsec <stronka.k@gmail.com>
2021-06-07 22:43:42 +0200

    Generalize multiple_lines heuristic

--- sudoku/logic/heuristics/multiple_lines.py
+++ sudoku/logic/heuristics/multiple_lines.py
@@ -6,13 +6,16 @@ def process_multiple_lines(stack: numpy.array):
     candidate_layer = stack[4, :, :]
     bands = (numpy.arange(0, 3), numpy.arange(3, 6), numpy.arange(6, 9))
 
-    for b1, b2, b3 in itertools.permutations(bands):
-        full_band = candidate_layer[numpy.array([b1, b2]).flatten(), 0:3]
-        for i, j in itertools.combinations(range(3), 2):
-            sub_band = full_band[:, (i, j)]
+    for cols in bands:
+        for row_box1, row_box2, row_box3 in itertools.permutations(bands):
+            scanned_indices = numpy.hstack((row_box1, row_box2))
 
-            if full_band.max() and full_band.sum() == sub_band.sum():
-                candidate_layer[b3[:, numpy.newaxis], (i, j)] = 0
+            full_band = candidate_layer[scanned_indices[:, numpy.newaxis], cols]
 
+            for i, j in itertools.combinations(cols, 2):
+                sub_band = candidate_layer[scanned_indices[:, numpy.newaxis], (i, j)]
+
+                if full_band.max() and full_band.sum() == sub_band.sum():
+                    candidate_layer[row_box3[:, numpy.newaxis], (i, j)] = 0
 
 
