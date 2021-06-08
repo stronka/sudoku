03976b62758a79b4ef1e3e9b586ae5c90d68317a
chrsec <stronka.k@gmail.com>
2021-06-08 21:20:22 +0200

    Implement solution logging in multiple lines heuristic

--- sudoku/logic/heuristics/multiple_lines.py
+++ sudoku/logic/heuristics/multiple_lines.py
@@ -1,16 +1,17 @@
+import copy
 import itertools
 import numpy
 
 _bands = (numpy.arange(0, 3), numpy.arange(3, 6), numpy.arange(6, 9))
 
 
-def process_multiple_lines(stack: numpy.array):
+def process_multiple_lines(stack: numpy.array, **kwargs):
     for candidate, candidate_layer in enumerate(stack):
-        _scan_row_bands_for_multiple_lines(candidate, candidate_layer)
-        _scan_column_bands_for_multiple_lines(candidate, candidate_layer)
+        _scan_row_bands_for_multiple_lines(candidate, candidate_layer, kwargs.setdefault('solution_log', None))
+        _scan_column_bands_for_multiple_lines(candidate, candidate_layer, kwargs.setdefault('solution_log', None))
 
 
-def _scan_column_bands_for_multiple_lines(candidate, candidate_layer):
+def _scan_column_bands_for_multiple_lines(candidate, candidate_layer, solution_log):
     for cols in _bands:
         for row_box1, row_box2, row_box3 in itertools.permutations(_bands):
             scanned_indices = numpy.hstack((row_box1, row_box2))
@@ -21,10 +22,21 @@ def _scan_column_bands_for_multiple_lines(candidate, candidate_layer):
                 sub_band = candidate_layer[scanned_indices[:, numpy.newaxis], (i, j)]
 
                 if full_band.max() and full_band.sum() == sub_band.sum():
+                    layer_before = copy.copy(candidate_layer)
                     candidate_layer[row_box3[:, numpy.newaxis], (i, j)] = 0
 
+                    if solution_log:
+                        removed = numpy.nonzero(layer_before-candidate_layer)
 
-def _scan_row_bands_for_multiple_lines(candidate, candidate_layer):
+                        for (n, m) in zip(removed[0], removed[1]):
+                            solution_log.add_step(
+                                (n, m),
+                                "Remove {}".format(candidate+1),
+                                "Multiple lines: candidates form lines along columns {} and {}".format(i, j)
+                            )
+
+
+def _scan_row_bands_for_multiple_lines(candidate, candidate_layer, solution_log):
     for rows in _bands:
         for col_box1, col_box2, col_box3 in itertools.permutations(_bands):
             scanned_indices = numpy.hstack((col_box1, col_box2))
@@ -35,4 +47,15 @@ def _scan_row_bands_for_multiple_lines(candidate, candidate_layer):
                 sub_band = candidate_layer[(i, j), scanned_indices[:, numpy.newaxis]]
 
                 if full_band.max() and full_band.sum() == sub_band.sum():
+                    layer_before = copy.copy(candidate_layer)
                     candidate_layer[(i, j), col_box3[:, numpy.newaxis]] = 0
+
+                    if solution_log:
+                        removed = numpy.nonzero(layer_before-candidate_layer)
+
+                        for (n, m) in zip(removed[0], removed[1]):
+                            solution_log.add_step(
+                                (n, m),
+                                "Remove {}".format(candidate+1),
+                                "Multiple lines: candidates form lines along rows {} and {}".format(i, j)
+                            )
--- sudoku/tests/unit/logic/heuristics/test_multiple_lines.py
+++ sudoku/tests/unit/logic/heuristics/test_multiple_lines.py
@@ -4,6 +4,7 @@ import numpy
 
 from sudoku.logic._solver import create_candidates_stack
 from sudoku.logic.heuristics.multiple_lines import process_multiple_lines
+from sudoku.logic.meta.solution_log import SolutionLog
 
 
 class MultipleLineHeuristicTest(unittest.TestCase):
@@ -171,3 +172,69 @@ class MultipleLineHeuristicTest(unittest.TestCase):
 
         process_multiple_lines(candidate_stack)
         numpy.testing.assert_equal(expected, candidate_stack[5, :, :])
+
+    def test_ProcessMultipleLines_CandidatesFormLinesAlongRows7And8Layer5_LogSolution(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[5, :, :] = numpy.array([
+            [6, 0, 0,   0, 6, 0,    0, 6, 0],
+            [0, 6, 0,   6, 0, 0,    0, 6, 6],
+            [0, 0, 6,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (0, 0),
+                'action': "Remove 6",
+                'reason': "Multiple lines: candidates form lines along rows 0 and 1"
+            },
+            {
+                'cell': (1, 1),
+                'action': "Remove 6",
+                'reason': "Multiple lines: candidates form lines along rows 0 and 1"
+            },
+        ]
+
+        process_multiple_lines(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
+
+    def test_ProcessMultipleLines_CandidatesFormLinesAlongCols3And5_LogSolution(self):
+        candidate_stack = create_candidates_stack()
+        solution_log = SolutionLog()
+        candidate_stack[4, :, :] = numpy.array([
+            [0, 0, 0,   5, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   5, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 5,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   5, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 5, 0,    0, 0, 0],
+        ])
+
+        expected = [
+            {
+                'cell': (7, 3),
+                'action': "Remove 5",
+                'reason': "Multiple lines: candidates form lines along columns 3 and 5"
+            },
+            {
+                'cell': (7, 5),
+                'action': "Remove 5",
+                'reason': "Multiple lines: candidates form lines along columns 3 and 5"
+            },
+        ]
+
+        process_multiple_lines(candidate_stack, solution_log=solution_log)
+        self.assertListEqual(expected, solution_log.get_steps())
