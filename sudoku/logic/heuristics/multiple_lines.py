4f9630d7a223b43e72d388ac05fb40dc32efbb0b
chrsec <stronka.k@gmail.com>
2021-06-08 20:55:42 +0200

    Implement row multiple_lines scan

--- sudoku/logic/heuristics/multiple_lines.py
+++ sudoku/logic/heuristics/multiple_lines.py
@@ -1,13 +1,18 @@
 import itertools
 import numpy
 
+_bands = (numpy.arange(0, 3), numpy.arange(3, 6), numpy.arange(6, 9))
+
 
 def process_multiple_lines(stack: numpy.array):
-    candidate_layer = stack[4, :, :]
-    bands = (numpy.arange(0, 3), numpy.arange(3, 6), numpy.arange(6, 9))
+    for candidate, candidate_layer in enumerate(stack):
+        _scan_row_bands_for_multiple_lines(candidate, candidate_layer)
+        _scan_column_bands_for_multiple_lines(candidate, candidate_layer)
 
-    for cols in bands:
-        for row_box1, row_box2, row_box3 in itertools.permutations(bands):
+
+def _scan_column_bands_for_multiple_lines(candidate, candidate_layer):
+    for cols in _bands:
+        for row_box1, row_box2, row_box3 in itertools.permutations(_bands):
             scanned_indices = numpy.hstack((row_box1, row_box2))
 
             full_band = candidate_layer[scanned_indices[:, numpy.newaxis], cols]
@@ -19,3 +24,15 @@ def process_multiple_lines(stack: numpy.array):
                     candidate_layer[row_box3[:, numpy.newaxis], (i, j)] = 0
 
 
+def _scan_row_bands_for_multiple_lines(candidate, candidate_layer):
+    for rows in _bands:
+        for col_box1, col_box2, col_box3 in itertools.permutations(_bands):
+            scanned_indices = numpy.hstack((col_box1, col_box2))
+
+            full_band = candidate_layer[rows, scanned_indices[:, numpy.newaxis]]
+
+            for i, j in itertools.combinations(rows, 2):
+                sub_band = candidate_layer[(i, j), scanned_indices[:, numpy.newaxis]]
+
+                if full_band.max() and full_band.sum() == sub_band.sum():
+                    candidate_layer[(i, j), col_box3[:, numpy.newaxis]] = 0
--- sudoku/tests/unit/logic/heuristics/test_multiple_lines.py
+++ sudoku/tests/unit/logic/heuristics/test_multiple_lines.py
@@ -72,3 +72,102 @@ class MultipleLineHeuristicTest(unittest.TestCase):
 
         process_multiple_lines(candidate_stack)
         numpy.testing.assert_equal(expected, candidate_stack[4, :, :])
+
+    def test_ProcessMultipleLines_CandidatesFormLinesAlongCols3And5_RemoveCandidatesInLastBox(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[4, :, :] = numpy.array([
+            [0, 0, 0,   5, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   5, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 5,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   5, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 5, 0,    0, 0, 0],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,   5, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 0, 5,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   5, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 5,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 5, 0,    0, 0, 0],
+        ])
+
+        process_multiple_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[4, :, :])
+
+    def test_ProcessMultipleLines_CandidatesFormLinesAlongCols7And8Layer5_RemoveCandidatesInFirstBox(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[5, :, :] = numpy.array([
+            [0, 0, 0,   0, 0, 0,    0, 6, 0],
+            [0, 0, 0,   0, 0, 0,    6, 6, 6],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 6],
+            [0, 0, 0,   0, 0, 0,    0, 6, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 6, 6],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    6, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 6],
+            [0, 0, 0,   0, 0, 0,    0, 6, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 6, 6],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+        ])
+
+        process_multiple_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[5, :, :])
+
+    def test_ProcessMultipleLines_CandidatesFormLinesAlongRows7And8Layer5_RemoveCandidatesInFirstBox(self):
+        candidate_stack = create_candidates_stack()
+        candidate_stack[5, :, :] = numpy.array([
+            [6, 0, 0,   0, 6, 0,    0, 6, 0],
+            [0, 6, 0,   6, 0, 0,    0, 6, 6],
+            [0, 0, 6,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+        ])
+
+        expected = numpy.array([
+            [0, 0, 0,   0, 6, 0,    0, 6, 0],
+            [0, 0, 0,   6, 0, 0,    0, 6, 6],
+            [0, 0, 6,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+            [0, 0, 0,   0, 0, 0,    0, 0, 0],
+        ])
+
+        process_multiple_lines(candidate_stack)
+        numpy.testing.assert_equal(expected, candidate_stack[5, :, :])
